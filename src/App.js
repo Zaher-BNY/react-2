073f1fb7b405fba7c40daeacb49fadb46e102ec1
Zaher-BNY <zaheryahia8@gmail.com>
2022-12-01 10:39:20 +0100

    react2 file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Adress from './components/Profile/Adress'
+import Fullname from './components/Profile/Fullname';
+import Profilephoto from './components/Profile/Profilephoto'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='body'> 
+    <Adress/>
+    <Fullname/>
+    <Profilephoto/>
+
+
+    
     </div>
   );
 }
---
+++ src/components/Profile/Fullname.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Fullname = () => {
+  return (
+    <div className='name'>Zaher Ben Yahia</div>
+  )
+}
+
+export default Fullname
+
